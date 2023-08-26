5f0fae0819c9d2d3bf64398f5c700a4cffde4488
Jayant Rohila <jrohila55@gmail.com>
2023-08-26 23:00:39 +0530

    update

--- app/(website)/page.js
+++ app/(website)/page.js
@@ -1,9 +1,8 @@
 import HomePage from "./home";
 import { getAllPosts } from "@/lib/sanity/client";
-
+export const runtime = 'edge' // 'nodejs' (default) | 'edge'
 export default async function IndexPage() {
   const posts = await getAllPosts();
   return <HomePage posts={posts} />;
 }
-
 export const revalidate = 10;
