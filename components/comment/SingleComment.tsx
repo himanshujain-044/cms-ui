9c70a4e292ba80fd1faba61c9da6499566f4b86d
Jayant Rohila <jrohila55@gmail.com>
2023-08-26 23:38:42 +0530

    update

--- components/comment/SingleComment.tsx
+++ components/comment/SingleComment.tsx
@@ -8,11 +8,11 @@ export default function Comment({ comment, firstParentId }) {
   const toggleReplyBox = () => setShowReplyBox(!showReplyBox)
   const formatDate = fullDate => {
     const date = fullDate?.split('T')[0]
-    const year = date.split('-')[0]
+    const year = date?.split('-')[0]
     const month = new Date(date).toLocaleDateString('default', {
       month: 'long'
     })
-    const day = date.split('-')[2]
+    const day = date?.split('-')[2]
 
     return `${day} ${month} ${year}`
   }
