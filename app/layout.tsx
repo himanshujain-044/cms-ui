737d22d9926f6bc4aca59e5fc95764e096e3f0a4
Jayant Rohila <jrohila55@gmail.com>
2023-08-27 12:09:29 +0530

    fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,12 @@
 import "@/styles/tailwind.css";
-import { Providers } from "./providers";
+import { Providers } from "../components/providers";
 import { cx } from "@/utils/all";
 import { Inter, Lora } from "next/font/google";
+import { getSettings } from "@/lib/sanity/client";
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
+import CookieBanner from "@/components/cookiebanner";
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter"
@@ -10,27 +15,40 @@ const lora = Lora({
   subsets: ["latin"],
   variable: "--font-lora"
 });
+export const revalidate = 10;
 
-export default function RootLayout({
+export default async function Layout({
   children
 }: {
   children: React.ReactNode;
 }) {
+  const settings = await getSettings();
   return (
     <html
       lang="en"
       suppressHydrationWarning
       className={cx(inter.variable, lora.variable)}>
-      {" "}
-      <meta
-        name="p:domain_verify"
-        content="23e3bb1046c70a90bcfe8630e33f96e3"
-      />
-      <head></head>
+      <head>
+        <meta
+          name="p:domain_verify"
+          content="23e3bb1046c70a90bcfe8630e33f96e3"
+        />
+        {process.env.NODE_ENV === "production" && (
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        )}
+        <GoogleAnalytics GA_MEASUREMENT_ID="G-W5ZKTJY3EP" />
+      </head>
       <body
         suppressHydrationWarning={true}
         className="text-gray-800 antialiased dark:bg-black dark:text-gray-400">
-        <Providers>{children}</Providers>
+        <Providers>
+          <Navbar {...settings} />
+          <div>{children}</div>
+          <Footer {...settings} />
+          <CookieBanner />
+        </Providers>
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
-import HomePage from "./home";
+import HomePage from "../components/home";
 import { getAllPosts } from "@/lib/sanity/client";
-export const runtime = 'edge' // 'nodejs' (default) | 'edge'
+export const runtime = "edge"; // 'nodejs' (default) | 'edge'
 export default async function IndexPage() {
   const posts = await getAllPosts();
   return <HomePage posts={posts} />;
